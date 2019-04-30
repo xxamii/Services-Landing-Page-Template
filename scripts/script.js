86045d2c0ca8db66db7123fcf85286a4de4b0c73
SSkelleRR <maximiakovliev@gmail.com>
2019-04-30 22:27:25 +0300

    completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,30 @@
 $(window).on('load' , function() {
 
+    //menu toggler
+
+    $('.navbar__burger').on('click' , function() {
+        $('.navbar__nav').slideToggle();
+    });
+
+    $(window).resize(function() {
+        if($(window).width() >= 768) {
+            $('.navbar__nav').show();   
+        }  else if($(window).width() <= 768) {
+            $('.navbar__nav').hide();
+        }
+    });
+
     // services accordion
 
     $('.services__service').on('click' , function() {
         var elem = $(this).children('.service__content');
 
-        if(elem.css('display') == 'flex') {
-            elem.css('display' , 'none');
-        } else {
-            elem.css('display' , 'flex');
-        }
+        elem.slideToggle('fast');
+        // if(elem.css('display') == 'flex') {
+        //     elem.css('display' , 'none');
+        // } else {
+        //     elem.css('display' , 'flex');
+        // }
     });
 
     // team photos
@@ -18,5 +33,23 @@ $(window).on('load' , function() {
         $('.photos__photo-current').removeClass('photos__photo-current');
         $(this).addClass('photos__photo-current');
         $('.team__name').html($(this).attr('name'));
+    });
+
+    // testimonials reviews-slider
+
+    $('.fa-chevron-up').on('click' , function() {
+        var current = $('.current');
+        if(current.prev().is('div')) {
+            current.prev().addClass('current');
+            current.removeClass('current'); 
+        }             
+    });
+
+    $('.fa-chevron-down').on('click' , function() {
+        var current = $('.current');
+        if(current.next().is('div')) {
+            current.removeClass('current');
+            current.next().addClass('current'); 
+        }       
     });
 });
